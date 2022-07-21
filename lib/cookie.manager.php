fefa22e57c43d6ebbb156745eab128390696e084
Arjun Nurdin <ganzide404@gmail.com>
2022-07-21 20:35:24 +0900

    CookieManager 1.0.0

--- lib/cookie.manager.php
+++ lib/cookie.manager.php
@@ -17,23 +17,12 @@ class CookieManager {
     private static $EXPIRES = 86400;// 3600 = 1 Jam;  86400 1 hari
     private static $PATH = "/";
     private static $DOMAIN = "113.14.15.14";
-    private static $SECURE = false;
-    private static $HTTPONLY = false;
-    private static $SAMESITE = 'None';
 
     public function __construct($names){
         $this->NAME = $names;
     }
 
     public function delate(){
-        /* $arr_cookie_options = array (
-                'expires' => time() - CookieManager::$EXPIRES,
-                'path' => CookieManager::$PATH,
-                'domain' => CookieManager::$DOMAIN, 
-                'secure' => CookieManager::$SECURE,   
-                'httponly' => CookieManager::$HTTPONLY, 
-                'samesite' => CookieManager::$SAMESITE
-            ); */
         $arr_cookie_options = array (
             'expires' => time() - CookieManager::$EXPIRES,
             'path' => CookieManager::$PATH,
